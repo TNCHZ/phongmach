cd9a8f86665785d2e3ff0d20560ed717e82c8899
TNCHZ <dochuong2222@gmail.com>
2024-11-22 19:51:23 +0700

    21/11

--- app/src/main/java/com/example/jpyou/User/UserFragments/RegisterForExaminationUserFragment.java
+++ app/src/main/java/com/example/jpyou/User/UserFragments/RegisterForExaminationUserFragment.java
@@ -9,13 +9,16 @@ import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.util.Log;
+import android.util.Printer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
@@ -26,12 +29,15 @@ import com.example.myapplication.R;
 
 
 public class RegisterForExaminationUserFragment extends Fragment {
-    PersonInformation ps = new UserInformation();
-    private String userID;
+    private PersonInformation ps;
+    private String userID, selectedItem;
+    private EditText txtName, txtDayOfBirth, txtPhone, txtEmail, txtDayRegis;
     private MyDatabaseHelper db;
+    private RadioButton rdMale, rdFemale;
+    private Button btnRegis;
     private String[] itemsDepartment;
     private Spinner spinner;
-    Context context;
+    private Context context;
 
     private ViewPager2 viewPager;
     private LinearLayout linearLayout;
@@ -50,21 +56,36 @@ public class RegisterForExaminationUserFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_user_register_for_examination, container, false);
         {
-            View view_UserInterface = inflater.inflate(R.layout.user_interface, container ,false);
-            viewPager = view_UserInterface.findViewById(R.id.viewPager_UserInterface);
-            linearLayout = view_UserInterface.findViewById(R.id.linearLayoutTop_UserInterface);
-            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) linearLayout.getLayoutParams();
-            params.removeRule(RelativeLayout.BELOW);
-            linearLayout.setLayoutParams(params);
+            ps = new PersonInformation();
+            db = new MyDatabaseHelper(getActivity());
+            ps = db.getInformation(userID);
+            txtName = view.findViewById(R.id.txtName_RegisterForExaminonUserFragment);
+            txtDayOfBirth = view.findViewById(R.id.txtDayOfBirth_RegisterForExaminonUserFragment);
+            txtPhone = view.findViewById(R.id.txtPhone_RegisterForExaminonUserFragment);
+            txtEmail = view.findViewById(R.id.txtEmail_RegisterForExaminonUserFragment);
+            rdMale = view.findViewById(R.id.rdMale_RegisterForExaminonUserFragment);
+            rdFemale = view.findViewById(R.id.rdFemale_RegisterForExaminonUserFragment);
+            txtDayRegis = view.findViewById(R.id.editTextDate_RegisterForExaminonUserFragment);
+
+            txtName.setText(ps.getHoTen());
+            txtDayOfBirth.setText(ps.getNgaySinh());
+            txtEmail.setText(ps.getEmail());
+            txtPhone.setText(ps.getSoDT());
+
+            if (ps.getGioiTinh().equals("Nam")) {
+                rdMale.setChecked(true);
+            } else {
+                rdFemale.setChecked(true);
+            }
 
 
             setUpSpinnerDepartment(view);
 
-            Button btnRegis = view.findViewById(R.id.btnUpdate_RegisterForExaminonUserFragment);
+            btnRegis = view.findViewById(R.id.btnUpdate_RegisterForExaminonUserFragment);
             btnRegis.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
+                    db.registerExamination(userID, txtDayRegis.getText().toString(), selectedItem);
                 }
             });
         }
@@ -81,7 +102,7 @@ public class RegisterForExaminationUserFragment extends Fragment {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String selectedItem = adapterView.getItemAtPosition(i).toString();
+                selectedItem = adapterView.getItemAtPosition(i).toString();
                 // Xử lý lựa chọn của người dùng
             }
 
